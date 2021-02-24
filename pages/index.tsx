dd919fe7aa01ae6ff5fcda7d8915b85e74d7ebfd
TiagoRodrigues456 <trodrigues322@gmail.com>
2021-02-24 10:45:36 -0300

    Component/Countdown and Start Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,27 @@
 import { CompletedChallenges } from '../src/components/CompletedChallenges'
+import { Countdown } from '../src/components/Countdown'
 import { ExperienceBar } from '../src/components/ExperienceBar'
 import { Profile } from '../src/components/Profile'
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css'
 
 
 export default function Home() {
   return (
+
     <div className={styles.container}>
+
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
