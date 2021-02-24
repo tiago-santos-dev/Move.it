bdbf37463c71fbefa639cf7d59e34b0ab4c54f83
TiagoRodrigues456 <trodrigues322@gmail.com>
2021-02-24 08:50:31 -0300

    Profile component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import Head from 'next/head'
 import { ExperienceBar } from '../src/components/ExperienceBar'
+import { Profile } from '../src/components/Profile'
+
+import styles from '../styles/pages/Home.module.css'
 
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
